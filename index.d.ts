591532fa22831b6261220b297d9b9162bd21cfa2
Mohamed Shaban <mohamed@8fit.com>
2019-03-22 15:54:28 +0100

    feat(*): add typescript type definitions

---
+++ index.d.ts
@@ -0,0 +1,58 @@
+// Type definitions for react-native-simple-markdown 1.1.0
+// Project: https://github.com/CharlesMangwa/react-native-simple-markdown
+// Definitions by: Mohamed Shaban <https://github.com/drmas>
+
+import { Component, ReactNode } from 'react';
+import { ViewStyle, TextStyle, ImageStyle, StyleProp } from 'react-native';
+
+export interface ReactNativeSimpleMarkdown { }
+export type ValidRules =
+  | 'autolink'
+  | 'blockQuote'
+  | 'br'
+  | 'codeBlock'
+  | 'del'
+  | 'em'
+  | 'heading'
+  | 'hr'
+  | 'image'
+  | 'inlineCode'
+  | 'link'
+  | 'list'
+  | 'newline'
+  | 'paragraph'
+  | 'strong'
+  | 'table'
+  | 'url';
+
+interface State {
+  key: string;
+  withinText: boolean;
+}
+
+export interface Node {
+  content: any;
+  target: string;
+  level: number;
+  items: any[];
+  ordered: boolean;
+}
+
+type Output = (content: any, state: State) => string;
+
+export interface Rule {
+  match?: any;
+  react?: (node: Node, output: Output, state: State) => ReactNode;
+}
+
+export type Rules = Partial<{ [key in ValidRules]: Rule }>;
+
+export interface Props {
+  styles?: { [key: string]: StyleProp<ViewStyle> | StyleProp<TextStyle> | StyleProp<ImageStyle> };
+  children?: string | string[];
+  rules?: Rules;
+  whitelist?: string[];
+  blacklist?: string[];
+}
+
+export default class RNSMDComponent extends Component<Props> {}
